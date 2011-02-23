248792f7221d2e243279abe635f3f1942da555bd
David Mike Simon <david.mike.simon@gmail.com>
2011-02-23 15:11:07 -0500

    Regex that handles extra spaces that wind up in there

--- lib/shutup_sqlite.rb
+++ lib/shutup_sqlite.rb
@@ -1,6 +1,6 @@
 class ActiveRecord::ConnectionAdapters::SQLiteAdapter
   # Aaaaargh
   def log_info(sql, name, ms)
-    super(sql, name, ms) unless sql.match(/^SELECT name FROM 'sqlite_master'/)
+    super(sql, name, ms) unless sql.match(/^\s*SELECT\s+name\s+FROM\s+sqlite_master\s+/)
   end
 end
