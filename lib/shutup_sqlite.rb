56638fb66e6b52d977e3f46fa4f52745b68e49ff
David Mike Simon <david.mike.simon@gmail.com>
2011-05-06 09:19:13 -0500

    Now works on both Rails 2 and 3

--- lib/shutup_sqlite.rb
+++ lib/shutup_sqlite.rb
@@ -1,10 +1,20 @@
 class ActiveRecord::ConnectionAdapters::SQLiteAdapter
-  # Aaaaargh
-  def execute(sql, name = nil) #:nodoc:
-    if sql.match(/^\s*SELECT\s+name\s+FROM\s+sqlite_master\s+/)
-      @connection.execute(sql)
-    else
-      log(sql, name) { @connection.execute(sql) }
+  def should_shutup?(sql)
+    # Aaaaargh, I hate these log entries SO MUCH
+    sql.match(/^\s*SELECT\s+name\s+FROM\s+sqlite_master\s+/)
+  end
+
+  if Rails::VERSION::MAJOR == 2
+    def log_info(sql, name, ms)
+      super unless should_shutup?(sql)
+    end
+  else
+    def execute(sql, name = nil) #:nodoc:
+      if should_shutup?(sql)
+        @connection.execute(sql)
+      else
+        log(sql, name) { @connection.execute(sql) }
+      end
     end
   end
 end
